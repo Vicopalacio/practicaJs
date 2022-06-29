4d2604ce4c65dfcc5d5ff857bfb96f068c1cb86e
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-06-29 18:32:46 -0300

    empezamos a comitear variables

--- js/1-variables.js
+++ js/1-variables.js
@@ -4,7 +4,7 @@
 de multiples
 lineas */
 
-//mostrar un mensaje en el documento html o pantalla
+//mostrar o imprimir un mensaje en el documento html o pantalla
 document.write("<h1>Hola mundo, año 2022, canción</h1>");
 
 //mostrar un mensaje por consola
@@ -20,4 +20,23 @@ console.log("alguna frase");
 document.write("<p>Resultado: " + (29+6) + "</p>");
 //cuando un signo + tiene texto de un lado o del otro directamente concatena es decir, unir texto, entonces aislo la operacion en otro parentesis.Agregamos p de parrafo, lo envolvemos entre comillas al otro </p> porque es texto y lo separamos con el + para que quede todo prolijo.
 
-//VARIABLES
+// crear o declarar o inicializar VARIABLES (var - let - const) var no se usa, usamos let o const
+//con let usamos variables que van a cambiar
+// con const constante usamos variables que no van a cambiar su valor
+
+let nombrePersona = "Cristian";
+let dolar = 238;
+
+document.write ("<p>Persona:"+ nombrePersona +"</p>")
+document.write ("<p>Play5: $"+(500* dolar) +" sin impuestos </p>")
+
+//actualizar el valor del dolar
+dolar = 220; 
+document.write ("<p>Play5: $"+(500* dolar) +" sin impuestos </p>");
+
+//pedimos al usuario que ingrese el valor del dolar
+dolar = parseInt(prompt ("Ingrese el valor del dolar")); 
+console.log(dolar);
+document.write ("<p>Play5: $"+(500* dolar) +" sin impuestos </p>")
+
+//parseFloat numero decimal y parseInt numero entero.
