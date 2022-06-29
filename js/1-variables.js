2feada3122fb201b2bfbe891558cc52238d9f285
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-06-29 18:07:51 -0300

    mostramos diferentes mensajes por pantalla, consola y alert

---
+++ js/1-variables.js
@@ -0,0 +1,23 @@
+//Esto es un comentario de una unica linea
+
+/*Esto es un comentario 
+de multiples
+lineas */
+
+//mostrar un mensaje en el documento html o pantalla
+document.write("<h1>Hola mundo, año 2022, canción</h1>");
+
+//mostrar un mensaje por consola
+console.log("alguna frase");
+
+//mostrar un mensaje por una ventana emergente
+// alert("esto es una ventana emergente");
+
+//mostrar por pantalla la suma de dos numeros 29+6
+// document.write("29+6"); esto no lo resuelve
+// document.write(26+6) //va sin comillas para realizar la operacion matematica
+//resultado: 35
+document.write("<p>Resultado: " + (29+6) + "</p>");
+//cuando un signo + tiene texto de un lado o del otro directamente concatena es decir, unir texto, entonces aislo la operacion en otro parentesis.Agregamos p de parrafo, lo envolvemos entre comillas al otro </p> porque es texto y lo separamos con el + para que quede todo prolijo.
+
+//VARIABLES
