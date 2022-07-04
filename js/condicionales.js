6c3da02f5836cbe8ae23a562cd75028a7f23c169
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-04 18:29:39 -0300

    agregamos mas condicionales

--- js/condicionales.js
+++ js/condicionales.js
@@ -8,18 +8,31 @@ if (condicion logica){
 si la rama no se desarrolla podemos usar el ELSE
 */
 
-let numero1 = parseInt(prompt("ingrese un primer numero"))
-let numero2 = parseInt(prompt("ingrese un segundo numero"))
+let numero1 = parseInt(prompt("ingrese un primer numero"));
+let numero2 = parseInt(prompt("ingrese un segundo numero"));
 
-console.log(numero1)
-console.log(numero2)
+console.log(numero1);
+console.log(numero2);
 
-if( numero1 > numero2){
-    document.write("El primer numero es el mayor, el numero ingresado: "+ numero1)
-}else{
-    if(numero2 > numero1){
-    document.write("El segundo numero es el mayor, el numero ingresado: "+ numero2)
-    }else{
-    document.write("Los numeros son iguales, el numero ingresado: "+ numero2)
-    }
-}
+// if (numero1 > numero2) {
+//   document.write(
+//     "El primer numero es el mayor, el numero ingresado: " + numero1
+//   );
+// } else if (numero2 > numero1) {
+//   document.write(
+//     "El segundo numero es el mayor, el numero ingresado: " + numero2
+//   );
+// } else {
+//   document.write("Los numeros son iguales, el numero ingresado: " + numero2);
+// }
+if (numero1 > numero2) {
+    document.write(
+      "El primer numero es el mayor, el numero ingresado: " + numero1
+    );
+  } else if (numero2 === numero1) {
+    document.write("Los numeros son iguales, el numero ingresado: " + numero2);
+  } else {
+    document.write(
+        "El segundo numero es el mayor, el numero ingresado: " + numero2
+      );
+  }
