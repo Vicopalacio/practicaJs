41e7f290c43c0fc23478e77d1bde39421a01e7e3
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-07-04 18:15:20 -0300

    agregamos condicionales

--- js/condicionales.js
+++ js/condicionales.js
@@ -2,6 +2,24 @@
 
 if (condicion logica){
     todo el codigo a ejecutar si se cumple la condicion, o si la condicion e verdadera true
-}
+}else{
 
+}
+si la rama no se desarrolla podemos usar el ELSE
 */
+
+let numero1 = parseInt(prompt("ingrese un primer numero"))
+let numero2 = parseInt(prompt("ingrese un segundo numero"))
+
+console.log(numero1)
+console.log(numero2)
+
+if( numero1 > numero2){
+    document.write("El primer numero es el mayor, el numero ingresado: "+ numero1)
+}else{
+    if(numero2 > numero1){
+    document.write("El segundo numero es el mayor, el numero ingresado: "+ numero2)
+    }else{
+    document.write("Los numeros son iguales, el numero ingresado: "+ numero2)
+    }
+}
